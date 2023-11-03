f8b7c7d788abac453bc5da3df4ad23f9db547ed2
Dava1991 <134635344+Dava1991@users.noreply.github.com>
2023-11-03 15:17:34 +0000

    Deploying to gh-pages from @ Dava1991/goit-js-hw-11@c15b753e6708f3aa8956aa241c11f2132e08386c 🚀

---
+++ index.324d608c.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".go-top");window.addEventListener("scroll",(function(){const o=window.pageYOffset,t=document.documentElement.clientHeight;o>t?e.classList.add("show-go-top"):e.classList.remove("show-go-top")})),e.addEventListener("click",(function e(){window.pageYOffset>0&&(window.scrollBy(0,-25),setTimeout(e,0))}));
+//# sourceMappingURL=index.324d608c.js.map
---
+++ index.f47f8121.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".go-top");window.addEventListener("scroll",(function(){var o=window.pageYOffset,t=document.documentElement.clientHeight;o>t?e.classList.add("show-go-top"):e.classList.remove("show-go-top")})),e.addEventListener("click",(function e(){window.pageYOffset>0&&(window.scrollBy(0,-25),setTimeout(e,0))}))}();
+//# sourceMappingURL=index.f47f8121.js.map
